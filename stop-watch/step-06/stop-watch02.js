d7fd99a2b94b93c1d37ad335991ec28476094118
tomerumardekar <tomtom1361991@gmail.com>
2022-08-21 21:11:19 +0300

    minuts and second

---
+++ stop-watch/step-06/stop-watch02.js
@@ -0,0 +1,97 @@
+const stopWatchView=document.querySelector('#stop-watch-view');
+
+const toggleStopWatchButton = document.getElementById('toggle-stop-watch');
+toggleStopWatchButton.addEventListener('click', toggleStopWatch);
+
+const reset=document.querySelector('#reset-stop-watch');
+reset.addEventListener('click',resetStopWatch);
+
+const lapsView = document.querySelector('#laps-view');
+
+const lapsButton = document.querySelector('#stop-watch-laps').addEventListener('click', addLap);
+
+let hundredth = 0;
+let seconds = 0;
+let minutes =0;
+
+function getTimeString(){
+  let secondsString = seconds;
+if(seconds<10){
+    secondsString='0' + seconds;
+}
+let minutesString =minutes;
+if(minutes<10){
+   minutesString='0' + minutes;
+}
+const timeString=`${minutesString}:${secondsString}:${hundredth}`;
+return timeString
+}
+
+function displayTime(){
+
+// option to present arguments
+
+   // stopWatchView.innerHTML = minutes+ ':' + seconds + ':'+ hundredth;
+/* let secondsString = seconds;
+if(seconds<10){
+    secondsString='0' + seconds;
+}
+
+let minutesString =minutes;
+if(minutes<10){
+   minutesString='0' + minutes;
+} */
+
+    stopWatchView.innerHTML=getTimeString();
+}
+
+function addLap(){
+  lapsView.innerHTML += `<li> ${getTimeString()}</li>`
+  
+}
+
+
+
+function resetStopWatch(){
+ hundredth = 0;
+ seconds = 0;
+ minutes = 0;
+displayTime();
+}
+
+
+function handleTimeChange(){
+    hundredth++;
+  if(hundredth > 99){
+    hundredth = 0;
+    seconds++;
+   
+    if(seconds > 59){
+    seconds = 0;
+    minutes++;
+  }
+}
+    displayTime();
+}
+
+function startStopWatch() {
+  intervalId = setInterval(handleTimeChange, 10);
+  toggleStopWatchButton.innerHTML = 'Stop';
+}
+
+function stopStopWatch() {
+  clearInterval(intervalId);
+  toggleStopWatchButton.innerHTML = 'Start';
+  intervalId = null;
+}
+
+let intervalId = null;
+function toggleStopWatch() {
+  if (intervalId == null) {
+    startStopWatch();
+  } else {
+    stopStopWatch();
+  }
+}
+
+
