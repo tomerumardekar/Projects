6d8fb6610c0dfd6dffe50411818ef62ffc68d05e
tomerumardekar <tomtom1361991@gmail.com>
2022-08-24 18:00:28 +0300

    export import

---
+++ app.js
@@ -0,0 +1,4 @@
+import { name } from './timers.js';
+console.log('tomer');
+import object from './timers.js';
+console.log('name barak');
---
+++ timers.js
@@ -0,0 +1,7 @@
+export const name = 'tomer';
+
+const obj={
+    name:'barak',
+  
+};
+export default obj;
